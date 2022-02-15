de6741ea83511bc236ad46f99119e7d69a40cb97
koo vemu <coolvm9@gmail.com>
2022-02-14 21:27:02 -0800

    Updates

--- SQSProcessorFunction/src/main/java/believe/App.java
+++ SQSProcessorFunction/src/main/java/believe/App.java
@@ -6,20 +6,23 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Handler;
 import java.util.stream.Collectors;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Handler for requests to Lambda function.
  */
 public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
-
+    private static final Logger logger = LogManager.getLogger(Handler.class);
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
@@ -28,15 +31,20 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
                 .withHeaders(headers);
         try {
-            LambdaLogger logger1 = context.getLogger();
-            logger1.log("--------------TeST--------------");
+            logger.info("--------------TeST--------------");
+            String host = System.getenv("dbHost");
+            String port = System.getenv("dbPort");
+
+            String test = System.getenv("PARAM1");
+            logger.info("======"+ test+" "+host+" "+port);
             final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-            String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
+            String output = String.format("{ \"message\": \"believe\", \"location\": \"%s\" }", pageContents);
             return response
                     .withStatusCode(200)
                     .withBody(output);
 
         } catch (IOException e) {
+            e.printStackTrace();
             return response
                     .withBody("{}")
                     .withStatusCode(500);
--- SQSProcessorFunction/src/test/java/believe/AppTest.java
+++ SQSProcessorFunction/src/test/java/believe/AppTest.java
@@ -16,8 +16,7 @@ public class AppTest {
     String content = result.getBody();
     assertNotNull(content);
     assertTrue(content.contains("\"message\""));
-    assertTrue(content.contains("\"hello world\""));
+    assertTrue(content.contains("\"believe\""));
     assertTrue(content.contains("\"location\""));
-//      assertTrue(1,1);
   }
 }
